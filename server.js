019f77eb7d9ed3493172bcae6c670ca47ca28e2a
vibha tiwari <tiwari.vibhatiwari1895@gmail.com>
2019-11-13 15:42:20 +0530

    added get /

--- server.js
+++ server.js
@@ -3,4 +3,12 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(port, () => console.log(`Example app listening on port !`))
+app.get('/product', (request, response) => {
+    const connection = connect()
+    const statement = `select * from Product`
+    connection.query(statement, (error, products) => {
+        connection.end()
+        response.send(products)
+    })
+})
